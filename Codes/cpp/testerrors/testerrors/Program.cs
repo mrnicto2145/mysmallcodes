78f0465df23e5ad975e45f4cf86d3948f92cb96c
mrnicto2145 <161124301+mrnicto2145@users.noreply.github.com>
2025-10-11 09:38:51 +0500

    moving test program here

---
+++ Codes/cpp/testerrors/testerrors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Slide01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Math.Round(5.0));
+        }
+
+        /*static int Min(int a, int b, int c)
+		{
+			return Math.Min(a, Math.Min(b, c));
+		}*/
+    }
+}
