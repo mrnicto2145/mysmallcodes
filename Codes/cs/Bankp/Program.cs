3ccd7daa150c8476fbe4c33243c7c585ff3ad0de
mrnicto2145 <161124301+mrnicto2145@users.noreply.github.com>
2025-10-19 15:58:23 +0500

    new commit

---
+++ Codes/cs/Bankp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Hello
+{
+    class Program
+    {
+        public static double Calculates(string userInput)
+        {
+	        string[] userInputs=userInput.Split();
+            double baseDebt = double.Parse(userInputs[0]);
+            double procent = double.Parse(userInputs[1]);
+            int timeInMonth = int.Parse(userInputs[2]);
+            return baseDebt * Math.Pow(1 + (procent/100) / 12, timeInMonth);
+        }
+        static void Main()
+        {
+            string userInput;
+            userInput = Console.ReadLine();
+            double ans = Calculates(userInput);
+            Console.WriteLine(ans);
+        }
+    }
+}
