43b126456d83f3509bddfa0332fe467d7c30b721
mrnicto2145 <161124301+mrnicto2145@users.noreply.github.com>
2025-11-04 19:42:56 +0500

    ok

---
+++ Codes/cs/ConsoleAppForTests/ClassLibraryForTests/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassLibraryForTests;
+
+public class Solver
+{
+    public static double[] Solve(double a, double b, double c)
+    {
+        var discriminant = b * b - 4 * a * c;
+        if (discriminant > 0)
+        {
+            var x1 = (-b + Math.Sqrt(discriminant)) / 2 / a;
+            var x2 = (-b - Math.Sqrt(discriminant)) / 2 / a; 
+            return new[] {x1, x2}; 
+        } 
+        else if (discriminant == 0)
+        {
+            var x = -b / 2 /a;
+            return new[] {x};
+        }
+        else
+        {
+            return new double[0];
+        }
+    }
+}
