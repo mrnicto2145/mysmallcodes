57fc0409e8e87aa736a1e372a32213d08e4a6b53
mrnicto2145 <161124301+mrnicto2145@users.noreply.github.com>
2025-11-04 19:43:52 +0500

    ok

---
+++ Codes/cs/ConsoleAppForTests/ConsoleAppForTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using ClassLibraryForTests;
+
+namespace Program;
+public class Program
+{
+    static void Main()
+    {
+        var a = double.Parse(Console.ReadLine());
+        var b = double.Parse(Console.ReadLine());
+        var c = double.Parse(Console.ReadLine());
+
+        var result = ClassLibraryForTests.Solver.Solve(a,b,c);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+    }
+}
