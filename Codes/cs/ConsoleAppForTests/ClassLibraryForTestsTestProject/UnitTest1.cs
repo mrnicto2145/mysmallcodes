105776a057aba589e15a2fc3d2a3862567a87ab6
mrnicto2145 <161124301+mrnicto2145@users.noreply.github.com>
2025-11-04 19:45:25 +0500

    ok

---
+++ Codes/cs/ConsoleAppForTests/ClassLibraryForTestsTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace myNamespace{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void OrdinaryTest()
+        {
+            double a = 1;
+            double b = -3;
+            double c = 2;
+            
+            var result = ClassLibraryForTests.Solver.Solve(a,b,c);
+
+            ClassicAssert.AreEqual(2, result[0]);
+            ClassicAssert.AreEqual(1, result[1]);        
+        }
+    }
+}
